02123ce6565efce23043d18bfd2c0a8f5f68c330
Harshal Patil(Original) <hpatil@rsimail.com>
2022-02-01 23:42:09 +0530

    suggester

--- src/AzureSearch.suites.Application/Search/DocumentSearchClient.cs
+++ src/AzureSearch.suites.Application/Search/DocumentSearchClient.cs
@@ -250,7 +250,7 @@ namespace CognitiveSearch.UI
                 {
                     UseFuzzyMatching = fuzzy,
                     Size = 100,
-                    SearchFields = { "name", "first_name", "last_name", "city", "address_line_1", "address_line_2", "email_address" }
+                    SearchFields = { "name", "first_name", "last_name", "city", "address_line_1", "address_line_2", "email_address", "CASE_TYPE" }
                 };
 
                 return _searchClient.Suggest<SearchDocument>(searchText, "sg", options);
